949bea7cc9aa0fffb6d65f5c663598d80239e4a8
ksenia <ksenia@localhost.localdomain>
2016-12-18 09:19:09 -0500

    Smarter way to resize

--- images/resize_image_to_cube.py
+++ images/resize_image_to_cube.py
@@ -6,14 +6,27 @@ import os
 im_path = sys.argv[1]
 im_dir = sys.argv[2]
 
+if not os.path.exists(im_dir):
+    os.makedirs(im_dir)
+
+h_big = 600
+w_big = 600
+    
 im = cv2.imread(im_path)
 h, w, g = im.shape
-base=np.empty((1000,1000,3),dtype=np.uint8)
+while h_big < h:
+	h_big += 100
+w_big = h_big
+while w_big < w:
+	w_big += 100
+h_big = w_big
+
+base=np.empty((h_big,w_big,3),dtype=np.uint8)
 base[:] = (255,255,255)
-x = (1024 - h)/2
-y = (1024 - w)/2
+x = (h_big - h)/2
+y = (w_big - w)/2
 base[x:h+x,y:w+y]=im
 base = cv2.resize(base, (128,128))
 im_name = os.path.basename(im_path)
-print im_dir + "/" + im_path
-cv2.imwrite(im_dir + "/" + im_name, base)
+print im_dir + im_name
+cv2.imwrite(im_dir + im_name, base)
