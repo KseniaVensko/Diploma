636e691cda7b069c045cb3dac38e5b5627579ca0
ksenia <ksenia@localhost.localdomain>
2016-12-18 09:16:52 -0500

    Add output_dir arg and 5 times per each image cycle

--- images/turn_images.py
+++ images/turn_images.py
@@ -5,13 +5,18 @@ import sys
 from scipy import ndimage
 
 directory = sys.argv[1]
+output_dir = sys.argv[2]
 
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 images_paths = [f for f in sorted(os.listdir(directory)) if f.endswith('.jpg') and not f.startswith('white1024cube')]
 print images_paths
 
 for path in images_paths:
-	im = cv2.imread(directory + path)
-	angle = np.random.random_integers(359)
-	rot = ndimage.rotate(im, angle, cval=255)
-	cv2.imwrite(directory + path, rot);
+	for i in range(5):
+		im = cv2.imread(directory + path)
+		angle = np.random.random_integers(359)
+		rot = ndimage.rotate(im, angle, cval=255)
+		name = os.path.splitext(path)[0] + str(i)
+		cv2.imwrite(output_dir + name + '.jpg', rot);
