e60fe3117c901caf8eab1c664381cc94399cdeff
ksenia <ksenia@localhost.localdomain>
2016-12-19 01:30:48 -0500

    moved funcs working with images to images_utils

--- simple_net.py
+++ simple_net.py
@@ -9,16 +9,30 @@ import keras.layers.advanced_activations as aa
 import socket
 import cv2
 from scipy import ndimage
+import argparse
+import os
+from string import digits
+from images_utils import draw_image, putLogo, create_blank
 
-white_cube = 'images/white1024cube.jpg'
-images_folder = 'images/'
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", type=str, default="images/training_im/")
+parser.add_argument("--result", type=str, default="images/sm_cropped/result.jpg")
+parser.add_argument("--port", type=int, default=7777)
+options = parser.parse_args()
+
+images_folder = vars(options)['dir']
+result_name = vars(options)['result']
+port = vars(options)['port']
+
+# images are 128x128, so we need canvas with size 128*sqrt(2)x128*sqrt(2). It will be about 182x182
+# but if we want to move images to other locations, we need, for example, 2xdimensions
+blank_side_size = 182*2
 
 def initialize_model():
 	epoch_size = 64*4
 	model = Sequential()
 	# input: h1 w1 h2 w2 h3 w3
 	model.add(Dense(12, init='normal', input_dim=6, activation='relu'))
-	#model.add(aa.LeakyReLU(alpha=0.3))
 	model.add(Dense(12, init='normal'))
 	model.add(Dense(9, init='normal', activation='relu'))
 	# output: x1 y1 a1 x2 y2 a2 x3 y3 a3
@@ -39,69 +53,53 @@ def initialize_sockets(port):
 def teaching(model, x, y):
 	x = x.reshape((1,-1))
 	y = y.reshape((1,-1))
-	print x
-	print y
-	model.train_on_batch(x, y)
+	print 'teaching ' + str(x) + ' ' + str(y)
+	#model.train_on_batch(x, y)
 	return model
 	
-def putLogo(logo, img1, x, y):
-    rows,cols,channels = logo.shape
-    roi = img1[x:rows+x, y:cols+y]
-    # Now create a mask of logo and create its inverse mask
-    logogray = cv2.cvtColor(logo,cv2.COLOR_BGR2GRAY)
-    # add a threshold
-    ret, mask = cv2.threshold(logogray, 220, 255, cv2.THRESH_BINARY_INV)
-    mask_inv = cv2.bitwise_not(mask)
-    # Now black-out the area of logo in ROI
-    img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-    # Take only region of logo from logo image.
-    logo_fg = cv2.bitwise_and(logo,logo,mask = mask)
-    dst = cv2.add(img1_bg,logo_fg)
-    img1[x:rows+x, y:cols+y] = dst
-
-def draw_image(name1, name2, name3, result_name, x1, y1, a1, x2, y2, a2, x3, y3, a3):
-	canvas = cv2.imread(white_cube)
-	img1 = cv2.imread(name1)
-	putLogo(ndimage.rotate(img1, 10, cval=255), canvas, x1, y1)
-	if name2 != None:
-		img2 = cv2.imread(name2)
-		putLogo(ndimage.rotate(img2, a2, cval=255), canvas, x2, y2)
-	if name3 != None:
-		img3 = cv2.imread(name3)
-		putLogo(ndimage.rotate(img3, 0, cval=255), canvas, x3, y3)
-	cv2.imwrite(images_folder + result_name, canvas);
-		
 def get_images_names():
-	return 'images/bear.jpg', 'images/sheep.jpg', 'images/walrus.jpg'
+	# TODO: get 3 different objects
+	images = [(images_folder + f) for f in sorted(os.listdir(images_folder)) if f.endswith('.jpg')]
+	return images[0], images[1], images[2]
 	
 def get_coordinates(name1, name2, name3):
-	im1 = cv2.imread(name1)
-	h1, w1, g = im1.shape
-	im2 = cv2.imread(name2)
-	h2, w2, g = im2.shape
-	im3 = cv2.imread(name3)
-	h3, w3, g = im3.shape
-	x = np.asarray([889, 742, 472, 598, 878, 720])
+	#~ im1 = cv2.imread(name1)
+	#~ h1, w1, g = im1.shape
+	#~ im2 = cv2.imread(name2)
+	#~ h2, w2, g = im2.shape
+	#~ im3 = cv2.imread(name3)
+	#~ h3, w3, g = im3.shape
+	x = np.asarray([128, 128, 128, 128, 128, 128])
 	# here should be predict = model.predict_on_batch(x)
+	# TODO: check that coordinates are correct
 	y = np.random.random_integers(360, size=9)
+	y = np.asarray([1,2,45,3,4,20,1,1,45])
 	print y
 	return y
 
 model = initialize_model()
-listening_sock, sending_sock = initialize_sockets(7777)
+listening_sock, sending_sock = initialize_sockets(port)
+print 'initialization complete'
 
 while True:
-#for i in range(10):
 	mes = listening_sock.recv(1024)
-	if mes.startswith('teaching'):
+	# simplenetteaching,h1,w1,h2,w2,h3,w3,x1,y1,a1,x2,y2,a2,x3,y3,a3
+	if mes.startswith('simplenetteaching'):
 		mes = mes.split(',')
-		x = np.asarray(map(int, mes[1:7]))		
+		x = np.asarray(map(int, mes[1:7]))
 		y = np.asarray(map(int, mes[7:]))
 		teaching(model, x, y)
+		data = 'simplenetsuccess'
+		sending_sock.sendto(data, ('<broadcast>', port))
+
 	elif mes.startswith('generate'):
 		n1,n2,n3 = get_images_names()
+		# TODO: check that image.size is (128, 128)
 		x1,y1,a1,x2,y2,a2,x3,y3,a3 = get_coordinates(n1, n2, n3)
-		draw_image(n1,n2,n3, 'result.jpg', x1,y1,a1,x2,y2,a2,x3,y3,a3)
+		# TODO: check that coordinates are correct
+		result_name = draw_image(n1,n2,n3,blank_side_size,images_folder,x1,y1,a1,x2,y2,a2,x3,y3,a3)
+		data = 'imagegenerated,' + result_name
+		sending_sock.sendto(data, ('<broadcast>', port))
 
 #x = np.random.random_integers(1024, size=6)
 #y = np.random.random_integers(360, size=9)
