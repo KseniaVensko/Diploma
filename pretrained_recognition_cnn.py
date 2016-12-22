faa8bc234618c7593726a3216ac3a2bba6d6857c
ksenia <ksenia@localhost.localdomain>
2016-12-22 00:09:55 -0500

    add interaction with agent

--- pretrained_recognition_cnn.py
+++ pretrained_recognition_cnn.py
@@ -1,6 +1,16 @@
 from keras.models import load_model
 import argparse
 import socket_utils
+import logger
+import numpy as np
+from keras.utils import np_utils
+from keras.preprocessing import image as image_utils
+import time
+
+my_name = "pretrained_recognition"
+coef = 0.8
+objects_count = 3
+objects = {}
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default="recognition_model.h5")
@@ -10,5 +20,62 @@ options = parser.parse_args()
 model_file = vars(options)['model']
 port = vars(options)['port']
 
+logger.write_to_log(my_name, "load sockets and model")
 listening_sock, sending_sock = socket_utils.initialize_sockets(port)
 model = load_model(model_file)
+logger.write_to_log(my_name, "sockets and model are loaded")
+keys = []
+with open('objects.txt') as f:
+        for line in f:
+            keys.append(line.rstrip('\n'))
+#~ nb_classes = len(keys)
+#~ values = range(nb_classes)
+#~ val = np_utils.to_categorical(values, nb_classes)
+#~ objects = dict(zip(keys, val))
+
+logger.write_to_log(my_name, "object keys" + str(keys))
+logger.write_to_log(my_name, "initialization complete")
+print "initialization complete"
+
+def decode_predictions(predict):
+	# predict is like [[ 0.2, 0.99, ...]]
+	predict = np.asarray(predict[0])
+	# get indices of objects, that have the probability higher, than coef
+	# TODO: ypu need to sort them
+	ob = [ n for n,i in enumerate(predict) if i>coef ][-objects_count:]
+	seen_objects = []
+	for i in ob:
+		seen_objects.append(keys[i])
+	
+	return seen_objects
+
+def preprocess_im(image_path):
+	image = image_utils.load_img(image_path)
+	image = image_utils.img_to_array(image)
+	image = np.expand_dims(image, axis=0)
+	image = image.astype('float32')
+	image /= 255
+	return image
+
+while True:
+	mes = listening_sock.recv(1024)
+	logger.write_to_log(my_name, "received mes " + mes)
+	if mes.startswith('objectteaching'):
+		mes = mes.split(',')
+		x = np.asarray(map(int, mes[1:4]))
+		y = np.asarray(map(int, mes[4:]))
+		teaching(model, x, y)
+		data = 'recognitionsuccess'
+		sending_sock.sendto(data, ('<broadcast>', port))
+
+	elif mes.startswith('recognize'):
+		mes = mes.split(',')
+		path = mes[1]
+		im = preprocess_im(path)
+		predict = model.predict_on_batch(im)
+		logger.write_to_log(my_name, "predict " + str(predict))
+		seen_objects = decode_predictions(predict)
+		logger.write_to_log(my_name, "seen objects " + str(seen_objects))
+		data = 'seenobjects:' + str(seen_objects)
+		sending_sock.sendto(data, ('<broadcast>', port))
+
