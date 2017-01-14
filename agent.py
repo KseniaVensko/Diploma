3c35275adbcc13971ec0c65244802a64eec8f31e
ksenia <ksenia@localhost.localdomain>
2017-01-14 07:39:43 -0500

    some refactoring

--- agent.py
+++ agent.py
@@ -12,11 +12,14 @@ port = vars(options)['port']
 my_name = "agent"
 listening_sock, sending_sock = socket_utils.initialize_sockets(port)
 
+def send_command(data):
+	sending_sock.sendto(data, ('<broadcast>', port))
+	mes = listening_sock.recv(1024)
+	mes = listening_sock.recv(1024)
+	return mes
+
 #while True:
-data = 'generate'
-sending_sock.sendto(data, ('<broadcast>', port))
-mes = listening_sock.recv(1024)
-mes = listening_sock.recv(1024)
+mes = send_command('generate')
 if mes.startswith('imagegenerated'):
 	mes = mes.split(',')
 	print mes[1]
@@ -26,24 +29,23 @@ else:
 	print 'another mes ' + mes
 
 logger.write_to_log(my_name, "asking to recognize image" + name)
-data = 'recognize,' + name
-sending_sock.sendto(data, ('<broadcast>', port))
-mes = listening_sock.recv(1024)
-mes = listening_sock.recv(1024)
+
+mes = send_command('recognize,' + name)
 if mes.startswith('seenobjects'):
 	mes = mes.split(':')
 	print mes[1]
-	objects = os.path.splitext(name)[0].split("_")
+	objects = os.path.splitext(os.path.basename(name))[0].split("_")
 	if (set(objects) != set(mes[1])):
 		logger.write_to_log(my_name, "correct objects " + str(objects) + "are not equal recognized objects" + str(mes[1]))
-		#~ data = 'simplenetteaching,10,11,22,23,34,35,16,17,18,29,291,292,393,394,395'
+		#~ data = 'generatingnetteaching,10,11,22,23,34,35,16,17,18,29,291,292,393,394,395'
+		#~ data = 'generatingnetteaching'
 		#~ sending_sock.sendto(data, ('<broadcast>', port))
 		#~ mes = listening_sock.recv(1024)
 		#~ mes = listening_sock.recv(1024)
-		#~ if mes.startswith('simplenetsuccess'):
+		#~ if mes.startswith('generatingnetsuccess'):
 			#~ print mes
 		#~ else:
 			#~ print 'another mes ' + mes
-		print "did not guess"
+		print "prediction was not correct"
 else:
 	print 'another mes ' + mes
