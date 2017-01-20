cf8751562dd2a120f6ea567d240be82ccbfed227
ksenia <ksenia@localhost.localdomain>
2017-01-20 05:53:35 -0500

    between hosts messaging

--- socket_utils.py
+++ socket_utils.py
@@ -1,5 +1,20 @@
 import socket
 
+def initialize_server_socket(port):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	s.bind(('', port))
+	
+	return s
+	
+def initialize_client_socket(port):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	
+	return s
+
 def initialize_sockets(port):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
