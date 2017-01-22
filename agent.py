4bd0f2b12bce79468572871f8b9f227925d25d36
ksenia <ksenia@localhost.localdomain>
2017-01-22 05:43:45 -0500

    some bug fixes

--- agent.py
+++ agent.py
@@ -26,12 +26,13 @@ def accept_connections():
 	i=0
 	while i < 2:
 		data, addr = s.recvfrom(1024)
-		if "recognize" in data:
+		if "recognize_net" in data:
 			recognition_addr = addr
 			i+=1
-		elif "generate" in data:
+		elif "generate_net" in data:
 			generating_addr = addr
 			i+=1
+	print i
 	return generating_addr, recognition_addr
 			
 def send_command_new(data, addr):
@@ -47,9 +48,11 @@ def send_command(data):
 
 generate_addr, recognize_addr = accept_connections()
 #while True:
-for i in range(4):
+for i in range(10):
+	print 'sending generate command'
 	mes = send_command_new(generate_command, generate_addr)
 	if mes.startswith(generate_success):
+		print 'received answer'
 		#~ mes = mes.split(',')
 		#~ print mes[1]
 		#~ name = mes[1]
@@ -58,24 +61,44 @@ for i in range(4):
 		print name
 		logger.write_to_log(log_file,my_name, "received generated image name " + name)
 	else:
-		print 'after generate another mes ' + mes
+		print 'after generate received another mes ' + mes
 		continue
 
+#	s.sendto(recognize_command + ',' + name, recognize_addr)
+	print 'sending recognize command'
 	mes = send_command_new(recognize_command + ',' + name, recognize_addr)
+	#mes,addr = s.recvfrom(1024)
+	if 'waiting' in mes:
+		socket_utils.send_image(name, recognize_addr, s)
+	else:
+		print 'after recognize received another mes ' + mes
+		continue
+	mes,addr = s.recvfrom(1024)
 	if mes.startswith(recognize_success):
+		print 'received predictions'
 		mes = mes.split(':')
 		print mes[1]
-		print name
 		objects = os.path.splitext(os.path.basename(name))[0].split("_")
-		print 'lala'
 		if mes[1] == []:
 			print "prediction was not correct"
+			logger.write_to_log(log_file,my_name, "correct objects " + str(objects) + "are not equal recognized objects " + 'empty')
+			print "sending teaching commands"
+			mes = send_command_new(generate_teach_command + ',' + str(True), generate_addr)
+			print mes
+			mes = send_command_new(recognize_teach_command + ',' + name + ',' + ','.join(objects), recognize_addr)
+			print mes
+			
 		elif set(objects) != set(mes[1]):
 			logger.write_to_log(log_file,my_name, "correct objects " + str(objects) + "are not equal recognized objects " + str(mes[1]))
+			print "sending teaching commands"
 			mes = send_command_new(generate_teach_command + ',' + str(True), generate_addr)
-                        mes = send_command_new(recognize_teach_command + ',' + name + ',' + ','.join(objects), recognize_addr)
+			print mes
+			mes = send_command_new(recognize_teach_command + ',' + name + ',' + ','.join(objects), recognize_addr)
+			print mes
 			print "prediction was not correct"
 		else:
+			print "sending generate teach command"
 			mes = send_command_new(generate_teach_command + ',' + str(False), generate_addr)
+			print mes
 	else:
-		print 'after recognize another mes ' + mes
+		print 'instead of predictions receives another mes ' + mes
