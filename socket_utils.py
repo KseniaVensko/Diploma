74fb7f88f61b8dc034029249c7aa1494f330a41b
root <root@localhost.localdomain>
2017-02-10 03:43:18 -0500

    remove unused code

--- socket_utils.py
+++ socket_utils.py
@@ -108,13 +108,3 @@ def receive_image(s):
 		f.write(im)
 
 	return name.strip()
-	
-def initialize_sockets(port):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	s.bind(('0.0.0.0', port))
-
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
-	return s, sock
